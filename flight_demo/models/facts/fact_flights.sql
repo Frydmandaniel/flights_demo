a56563294ccaf08615d09c809493bce573be7e3d
Daniel <danielfry36@gmail.com>
2023-06-08 20:18:45 +0300

    last commit

--- flight_demo/models/facts/fact_flights.sql
+++ flight_demo/models/facts/fact_flights.sql
@@ -16,7 +16,11 @@ with flights as (
 select distinct fl.flight_id,fl.flight_no,fl.aircraft_code ,fl.departure_airport ,fl.arrival_airport ,fl.status,flight_duration ,flight_duration_expected ,
 (case when flight_duration_expected > flight_duration then 'longer'
  	  when flight_duration_expected < flight_duration then 'shorter' else 'expected' end) as compared_expected_actual,
+	  		(case when dim_aircrafts.aircraft_code is not null then dim_aircrafts.aircraft_code else '-1' end) as aircraft_code_check,
+			(case when da.airport_code is not null then da.airport_code else '-1' end) as airport_code_check,
+			(case when dai.airport_code is not null then dai.airport_code else '-1' end) as second_airport_code_check,
        '{{ run_started_at.strftime ("%Y-%m-%d %H:%M:%S")}}'::timestamp as dbt_time
 from flights fl 
 join dim_aircrafts on fl.aircraft_code = dim_aircrafts.aircraft_code
-join dim_airports on fl.arrival_airport = dim_airports.airport_code
+join dim_airports da on fl.arrival_airport = da.airport_code
+join dim_airports dai on fl.departure_airport  = dai.airport_code
